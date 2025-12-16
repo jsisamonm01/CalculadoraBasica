269177a0a4a4505744ccf738f82973c3bbecc4d1
Javier Sisamon <jsisamonm01@educantabria.es>
2025-12-16 12:10:34 +0100

    Version 1.1 (un poco mejorada)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,23 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.InputMismatchException;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Primer numero?");
         try {
-            System.out.println("Primer numero?");
             int x = sc.nextInt();
             System.out.println("Segundo numero?");
             int y = sc.nextInt();
-            System.out.println("Operacion? (+ - / *)");
-            char op = sc.next().charAt(0);
-            int z;
-            switch (op) {
-                case '+':
-                    z = x + y;
-                    break;
-                case '-':
-                    z = x - y;
-                    break;
-                case '/':
-                    z = x / y;
-                    break;
-                case '*':
-                    z = x * y;
-                    break;
-                default:
-                    throw new IllegalArgumentException();
-            }
+            System.out.println("Operacion? (+/-)");
+            String op = sc.next();
+            int z = switch (op) {
+                case "+"-> x + y;
+                case "-" -> x - y;
+                default -> throw new InputMismatchException();
+            };
             System.out.println(x + op + y + "=" + z);
-        } catch (ArithmeticException e) {
-            System.out.println("Error: división por 0");
         } catch (InputMismatchException e) {
-            System.out.println("Error: argumento no valido (solo valen numeros)");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Error: operacion no valida");
+            System.out.println("Error: valor no valido!");
         }
     }
-} 
+}
